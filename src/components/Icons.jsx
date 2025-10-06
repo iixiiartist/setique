4945259e67ee2ad22ad8dff706fe7f275873b9fa
iixii <user@example.com>
2025-10-05 23:06:45 -0400

    Add missing AlertTriangle icon for deletion request modal

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -241,3 +241,11 @@ export const AlertCircle = ({ className }) => (
     <line x1="12" x2="12.01" y1="16" y2="16" />
   </Icon>
 )
+
+export const AlertTriangle = ({ className }) => (
+  <Icon className={className}>
+    <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z" />
+    <line x1="12" x2="12" y1="9" y2="13" />
+    <line x1="12" x2="12.01" y1="17" y2="17" />
+  </Icon>
+)
